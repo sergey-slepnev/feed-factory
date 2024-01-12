73494e4eef380ee35c05e459191afa0b63fa1f70
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-12 17:44:37 +0800

    initial commit

---
+++ src/main/java/ssp/springframework/petfeed/PetFeedApplication.java
@@ -0,0 +1,13 @@
+package ssp.springframework.petfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssp/springframework/petfeed/PetFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package ssp.springframework.petfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
